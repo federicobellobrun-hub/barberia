824fa876a9364bf52a1d28191cb4f18b4fe2dc76
federicobellobrun-hub <federicobellobrun@gmail.com>
2026-09-09 12:45:11 -0300

    Update icon.tsx

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,27 +1,27 @@
 import { ImageResponse } from "next/og";
 
+export const runtime = "edge";
 export const size = { width: 32, height: 32 };
 export const contentType = "image/png";
 
-export default function Icon() {
+export default async function Icon() {
   return new ImageResponse(
     (
       <div
         style={{
+          fontSize: 20,
+          background: "#F5F0E8",
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#F5F0E8",
           color: "#1C1712",
-          fontSize: 18,
-          fontWeight: 600,
         }}
       >
         R
       </div>
     ),
-    { ...size }
+    { width: 32, height: 32 }
   );
 }
